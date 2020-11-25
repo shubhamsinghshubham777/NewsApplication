0c1a4022e28977b3d739fd5587e170979970daf4
shubham <shubhamsinghshubham777@gmail.com>
2020-11-25 23:52:12 +0530

    DAO & Article Setup

---
+++ app/src/main/java/com/shubham/newsapplication/db/ArticleDao.kt
@@ -0,0 +1,18 @@
+package com.shubham.newsapplication.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.shubham.newsapplication.Article
+
+@Dao
+interface ArticleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    @Query("select * from articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
