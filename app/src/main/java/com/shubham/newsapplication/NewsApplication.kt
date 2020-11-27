89cb8a04a9ce9a11dcb1552f51d756ce1a278716
shubham <shubhamsinghshubham777@gmail.com>
2020-11-27 22:38:47 +0530

    No internet checking

---
+++ app/src/main/java/com/shubham/newsapplication/NewsApplication.kt
@@ -0,0 +1,5 @@
+package com.shubham.newsapplication
+
+import android.app.Application
+
+class NewsApplication : Application()
--- app/src/main/java/com/shubham/newsapplication/ui/NewsViewModelProviderFactory.kt
+++ app/src/main/java/com/shubham/newsapplication/ui/NewsViewModelProviderFactory.kt
@@ -1,14 +1,17 @@
 package com.shubham.newsapplication.ui
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.shubham.newsapplication.repository.NewsRepository
 
 class NewsViewModelProviderFactory(
-    val newsRepository: NewsRepository
+    val app: Application,
+    val newsRepository: NewsRepository,
+
 ): ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return NewsViewModel(newsRepository) as T
+        return NewsViewModel(app, newsRepository) as T
     }
 }
